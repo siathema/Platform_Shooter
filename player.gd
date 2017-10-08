cdc98cbba06d8e0b9322a1057961223b70a110ea
Matthias Linford <nate.holaday@gmail.com>
2017-10-07 21:02:14 -0700

    Added Jumping and state machine in player

--- player.gd
+++ player.gd
@@ -1,9 +1,16 @@
 extends KinematicBody2D
 
-const GRAVITY = 400.0
-const WALK_SPEED = 200.0
+const GRAVITY = 1000.0
+const WALK_SPEED = 60.0
+const MAX_WALK_SPEED = 600.0
+const JUMP_SPEED = 600.0
+
+enum {
+	ON_GROUND, FALLING, JUMPING, STANDING, WALKING_RIGHT, WALKING_LEFT
+}
 
 var velocity = Vector2()
+var state = ON_GROUND
 
 func _ready():
 	set_fixed_process(true)
@@ -11,17 +18,31 @@ func _ready():
 func _fixed_process(delta):
 	
 	velocity.y += delta * GRAVITY
-	if(Input.is_action_pressed("ui_left")):
-		velocity.x = -WALK_SPEED;
-	elif(Input.is_action_pressed("ui_right")):
-		velocity.x = WALK_SPEED;
-	else:
-		velocity.x = 0
 	
+	if(Input.is_action_pressed("ui_up")):
+		if(state != JUMPING and state != FALLING):
+			state = JUMPING
+	
+	if(Input.is_action_pressed("ui_left")):
+		if(velocity.x >= -MAX_WALK_SPEED):
+			velocity.x += -WALK_SPEED;
+	elif(Input.is_action_pressed("ui_right")):
+		if(velocity.x <= MAX_WALK_SPEED):
+			velocity.x += WALK_SPEED;
+	else:
+		if(state == ON_GROUND):
+			velocity.x = 0
+	
+	# state machine stuff
+	if(state == JUMPING): #Jumping code
+		velocity.y -= JUMP_SPEED
+		state = FALLING
+
 	var motion = velocity * delta
 	motion = move(motion)
 	
 	if(is_colliding()):
+		state = ON_GROUND
 		var n = get_collision_normal()
 		motion = n.slide(motion)
 		velocity = n.slide(velocity)
